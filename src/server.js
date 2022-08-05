56fdd45c8998442c8a74e1bf7d753814ed969532
L9628 <mhp4718@gmail.com>
2022-08-05 18:36:17 +0900

    Admin Panel

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from "http";
 import SocketIO from "socket.io";
+// import { instrument } from "@socket.io/admin-ui";
 import express from "express";
 
 const app = express();
@@ -12,6 +13,15 @@ app.get("/*", (req, res) => res.redirect("/"));
 
 const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
+// const wsServer = new Server(httpServer, {
+//   cors: {
+//     origin: ["https://admin.socket.io"],
+//     credentials: true,
+//   },
+// });
+// instrument(wsServer, {
+//   auth: false,
+// });
 
 function publicRooms() {
   const {
